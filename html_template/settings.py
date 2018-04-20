a602418848b372c1287ccbed62a1dbda519e9460
AuraHuang <aurahuang1996@gmail.com>
2018-04-20 00:14:43 -0700

    for heroku uploading

--- html_template/settings.py
+++ html_template/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ndt4yqjc@vnpj=^fkbbqrgf8$5+(gkf&)vpxdzgi4kweao*dyu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 

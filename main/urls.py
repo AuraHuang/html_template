7f094db455178b769b3f0f5668787184f367715f
AuraHuang <aurahuang1996@gmail.com>
2018-04-26 04:48:06 -0700

    update

--- main/urls.py
+++ main/urls.py
@@ -4,16 +4,16 @@ from . import views
 
 urlpatterns = [
 
-    path('index', views.index, name='index'),
+    path('/index', views.index, name='index'),
 
-    path('works_details', views.works_details, name='works_details'),
+    path('/works_details', views.works_details, name='works_details'),
 
-    path('about', views.about, name='about'),
+    path('/about', views.about, name='about'),
 
-    path('blog', views.blog, name='blog'),
+    path('/blog', views.blog, name='blog'),
 
-    path('blog_details', views.blog_details, name='blog_details'),
+    path('/blog_details', views.blog_details, name='blog_details'),
 
-    path('contact', views.contact, name='contact'),
+    path('/contact', views.contact, name='contact'),
 
 ]

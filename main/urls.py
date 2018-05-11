93f564fe85ee32cef124a45fffaaed7641aa7168
AuraHuang <aurahuang1996@gmail.com>
2018-05-10 23:56:26 -0700

    revise

--- main/urls.py
+++ main/urls.py
@@ -4,15 +4,15 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
 
-    path('works_details/', views.works_details, name='works_details'),
+    path('index/works_details/', views.works_details, name='works_details'),
 
     path('about/', views.about, name='about'),
 
     path('blog/', views.blog, name='blog'),
 
-    path('blog_details/', views.blog_details, name='blog_details'),
+    path('blog/blog_details/', views.blog_details, name='blog_details'),
 
     path('contact/', views.contact, name='contact'),
 

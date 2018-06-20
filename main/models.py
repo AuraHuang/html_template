7271be44673877b63acd5ae4782c5fc440fea0b5
JameStock <a6898208@gmail.com>
2018-06-20 08:50:00 -0700

    filter update

--- main/models.py
+++ main/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 class items(models.Model):
     # 股票代碼
-    items_code = models.IntegerField(default=0)
+    items_code = models.IntegerField(default=0, verbose_name='股票代碼')
     # 名字
-    items_name = models.CharField(max_length=200)
+    items_name = models.CharField(max_length=200, verbose_name='名字')
     # # 產業類別
-    items_category = models.IntegerField(default=0)
+    items_category = models.IntegerField(default=0, verbose_name='產業類別')
     # # 前一天收盤價
-    items_Cprice = models.DecimalField(max_digits=6, decimal_places=2)
+    items_Cprice = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='前一天收盤價')
     # # 預測期間
-    items_time = models.IntegerField(default=0)
+    items_time = models.IntegerField(default=0, verbose_name='預測期間')
     # # 預測收盤價
-    items_Pprice = models.DecimalField(max_digits=6, decimal_places=2)
+    items_Pprice = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='預測收盤價')
     # # 預測報酬率
-    items_rtn = models.DecimalField(max_digits=6, decimal_places=2)
+    items_rtn = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='預測報酬率')
 
     def __str__(self):
         return self.items_name
--- main/static/main/js/condition.js
+++ main/static/main/js/condition.js
@@ -1,106 +1,182 @@
 $(document).ready(function() {
 
-var x = true;
+// var x = true;
 
-$("#item_btn").click(function(){
+// $("#item_btn").click(function(){
 
-    if (x == true){
+//     if (x == true){
 
     	
-    	for (i = 1; i < 6; i++) {
+//     	for (i = 1; i < 6; i++) {
 
-   			$('#item_num').append("<li class='dropdown_list'>" + i +"</li>");
+//    			$('#item_num').append("<li class='dropdown_list'>" + i +"</li>");
 
-   		}
+//    		}
 
-   		$(".dropdown_list").each(function(){
-	   		$(this).click(function(){
-				$("#item_btn").text($(this).text());
-				$('.dropdown_list').remove();
-				x = true;
-			});
+//    		$(".dropdown_list").each(function(){
+// 	   		$(this).click(function(){
+// 				$("#item_btn").text($(this).text());
+// 				$('.dropdown_list').remove();
+// 				x = true;
+// 			});
 
-		});
+// 		});
 		
-		x = false;
+// 		x = false;
 
-    }else{
+//     }else{
 
-    	$('.dropdown_list').remove();
-    	x = true;
+//     	$('.dropdown_list').remove();
+//     	x = true;
 
-    }
-});
+//     }
+// });
 
-$("#rtn_btn").click(function(){
+// $("#rtn_btn").click(function(){
 
-    if (x == true){
+//     if (x == true){
 
     	
-    	for (i = 0; i < 6; i++) {
+//     	for (i = 0; i < 6; i++) {
 
-    		if (i == 5){
+//     		if (i == 5){
 
-    			$('#rtn_list').append("<li class='dropdown_list'>" + (i*10) + "% 以上 " + "</li>");
+//     			$('#rtn_list').append("<li class='dropdown_list'>" + (i*10) + "% 以上 " + "</li>");
 
-    		}else{
+//     		}else{
 
-    			$('#rtn_list').append("<li class='dropdown_list'>" + (i*10) + "% - " + ((i*10)+10) + "%" +"</li>");
+//     			$('#rtn_list').append("<li class='dropdown_list'>" + (i*10) + "% - " + ((i*10)+10) + "%" +"</li>");
 
-    		}
+//     		}
    			
 
-   		}
+//    		}
 
-   		$(".dropdown_list").each(function(){
-	   		$(this).click(function(){
-				$("#rtn_btn").text($(this).text());
-				$('.dropdown_list').remove();
-				x = true;
-			});
+//    		$(".dropdown_list").each(function(){
+// 	   		$(this).click(function(){
+// 				$("#rtn_btn").text($(this).text());
+// 				$('.dropdown_list').remove();
+// 				x = true;
+// 			});
 
-		});
+// 		});
 		
-		x = false;
+// 		x = false;
 
-    }else{
+//     }else{
 
-    	$('.dropdown_list').remove();
-    	x = true;
+//     	$('.dropdown_list').remove();
+//     	x = true;
 
-    }
-});
+//     }
+// });
 
-$("#time_btn").click(function(){
+// $("#time_btn").click(function(){
 
-    if (x == true){
+//     if (x == true){
 
     	
-    	for (i = 0; i < 4; i++) {
+//     	for (i = 0; i < 4; i++) {
 
-    		var timeList = ['單週','雙週','1個月','1季'];
-   			$('#time_list').append("<li class='dropdown_list'>" + timeList[i] +"</li>");
+//     		var timeList = ['單週','雙週','1個月','1季'];
+//    			$('#time_list').append("<li class='dropdown_list'>" + timeList[i] +"</li>");
 
-   		}
+//    		}
 
-   		$(".dropdown_list").each(function(){
-	   		$(this).click(function(){
-				$("#time_btn").text($(this).text());
-				$('.dropdown_list').remove();
-				x = true;
-			});
+//    		$(".dropdown_list").each(function(){
+// 	   		$(this).click(function(){
+// 				$("#time_btn").text($(this).text());
+// 				$('.dropdown_list').remove();
+// 				x = true;
+// 			});
 
-		});
+// 		});
 		
-		x = false;
+// 		x = false;
 
-    }else{
+//     }else{
 
-    	$('.dropdown_list').remove();
-    	x = true;
+//     	$('.dropdown_list').remove();
+//     	x = true;
 
-    }
+//     }
+// });
+
+// $("#industry_cate").click(function(){
+
+//     if (x == true){
+
+      
+//       for (i = 0; i < 4; i++) {
+
+//         var timeList = ['單週','雙週','1個月','1季'];
+//         $('#time_list').append("<li class='dropdown_list'>" + timeList[i] +"</li>");
+
+//       }
+
+//       $(".dropdown_list").each(function(){
+//         $(this).click(function(){
+//         $("#time_btn").text($(this).text());
+//         $('.dropdown_list').remove();
+//         x = true;
+//       });
+
+//     });
+    
+//     x = false;
+
+//     }else{
+
+//       $('.dropdown_list').remove();
+//       x = true;
+
+//     }
+// });
+
+$("#submit_btn").click(function () {
+      var day = $("#time_btn").val();
+      var return_rates = $("#rtn_btn").val();
+
+      $.ajax({
+        type:'GET',
+        url: '/ajax/filter/',
+        data: {
+          'day': day,
+          'return_rate': return_rates
+        },
+        success: function (data) {
+          // console.log(data.filt_result)
+          
+          
+          // ### 更新前端UI
+          results = $.parseJSON(data.filt_result);
+          console.log(results.length)
+          // 判斷搜尋是否有資料
+          if (results != 0) { //收尋到資料
+            // console.log(r[0].fields.items_name)
+            for(var i=0; i<results.length; i++){
+              contents = results[i].fields
+
+              console.log(contents.items_name)
+              console.log(contents.items_code)
+              console.log(contents.items_category)
+              console.log(contents.items_time)
+              console.log(contents.items_rtn)
+              console.log('******************')
+            }
+            
+          }else{ //搜尋失敗，沒找到資料
+            alert("沒有找到符合的搜尋條件.");
+          }
+        }
+      });
+
+    });
+
 });
 
-});
+
+
+
+
 

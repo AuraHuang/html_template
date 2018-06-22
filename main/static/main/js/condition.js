ef810a4e06d13aa345c8d383e96d71541d288b6c
AuraHuang <aurahuang1996@gmail.com>
2018-06-21 22:26:56 -0700

    change

--- main/static/main/js/condition.js
+++ main/static/main/js/condition.js
@@ -189,6 +189,82 @@ $(document).ready(function() {
     $("#filt_link").attr('href', links);
   });
 
+  $('.item_industry').each(function(){
+      if ($(this).text() == 01) {
+        $(this).text("水泥工業")
+      }else if ($(this).text() == 02){
+        $(this).text("食品工業")
+      }else if ($(this).text() == 03){
+        $(this).text("塑膠工業")
+      }else if ($(this).text() == 04){
+        $(this).text("紡織工業")
+      }else if ($(this).text() == 05){
+        $(this).text("電機機械")
+      }else if ($(this).text() == 06){
+        $(this).text("電器電纜")
+      }else if ($(this).text() == 21){
+        $(this).text("化學工業")
+      }else if ($(this).text() == 22){
+        $(this).text("生技醫療")
+      }else if ($(this).text() == 08){
+        $(this).text("玻璃陶瓷")
+      }else if ($(this).text() == 09){
+        $(this).text("造紙工業")
+      }else if ($(this).text() == 10){
+        $(this).text("鋼鐵工業")
+      }else if ($(this).text() == 11){
+        $(this).text("橡膠工業")
+      }else if ($(this).text() == 12){
+        $(this).text("汽車工業")
+      }else if ($(this).text() == 24){
+        $(this).text("半導體業")
+      }else if ($(this).text() == 25){
+        $(this).text("電腦及週邊設備業")
+      }else if ($(this).text() == 26){
+        $(this).text("光電業")
+      }else if ($(this).text() == 27){
+        $(this).text("通訊網路業")
+      }else if ($(this).text() == 28){
+        $(this).text("電子零組件業")
+      }else if ($(this).text() == 29){
+        $(this).text("電子通路業")
+      }else if ($(this).text() == 30){
+        $(this).text("資訊服務業")
+      }else if ($(this).text() == 31){
+        $(this).text("其他電子業")
+      }else if ($(this).text() == 14){
+        $(this).text("建材營造")
+      }else if ($(this).text() == 15){
+        $(this).text("航運")
+      }else if ($(this).text() == 16){
+        $(this).text("觀光")
+      }else if ($(this).text() == 17){
+        $(this).text("金融")
+      }else if ($(this).text() == 18){
+        $(this).text("貿易百貨")
+      }else if ($(this).text() == 23){
+        $(this).text("油電燃氣")
+      }else if ($(this).text() == 19){
+        $(this).text("綜合")
+      }else if ($(this).text() == 20){
+        $(this).text("其他")
+      }else if ($(this).text() == 80){
+        $(this).text("管理股票（由櫃買中心管理）")
+      }
+  });
+
+  $('.item_date').each(function(){
+      if ($(this).text() == 5) {
+        $(this).text("預測天數：單週")
+      }else if ($(this).text() == 10){
+        $(this).text("預測天數：雙週")
+      }else if ($(this).text() == 20){
+        $(this).text("預測天數：1個月")
+      }else if ($(this).text() == 60){
+        $(this).text("預測天數：1季")
+      }
+  });
+
 });
 
 
